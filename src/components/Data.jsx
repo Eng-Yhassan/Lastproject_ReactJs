3a390289c0f4f7155b01a1b0da97714bce2dec25
Yoonis <yoonkagallad2021@gmail.com>
2025-08-25 07:44:09 +0300

    last-project

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,6 +3,13 @@ import imag2 from "../images/Leadership.jpg"
 import imag3 from "../images/Charity-Gala.jpg"
 import imag4 from "../images/Digital.webp"
 import imag5 from "../images/Corporate.jpg"
+import imag6 from "../images/financial.jpg"
+import imag7 from "../images/ai-somalia.jpg"
+import imag8 from "../images/public-speak.jpg"
+import imag9 from "../images/wedding.jpg"
+import imag10 from "../images/innovation.jpg"
+import imag11 from "../images/musical festival.jpg"
+import imag12 from "../images/data analysis.jpg"
 
 const Data= [
     {
@@ -49,7 +56,70 @@ const Data= [
         category: "Events",
         date: "April 23, 2025",
         location: "Howl-Wadaag"
-    }
+    },
+     {
+        id:6,
+        image: imag6,
+        name: "Financial Planing Seminar",
+        title: "Learn investment strategies and financial planing from certfied advisor...",
+        category: "Seminar",
+        date: "October 03, 2025",
+        location: "Wasaarada-Maaliyada"
+    },
+    {
+        id:7,
+        image: imag7,
+        name: "Product Launch Event",
+        title: "Exclusive unvelling of innovative products with live demonstrations and networking ..",
+        category: "Event",
+        date: "November 13, 2025",
+        location: "Simad-Ilab"
+    },
+    {
+        id:8,
+        image: imag8,
+        name: "Public Speaking",
+        title: "Overcome stage fright and develop confident public speaking skills..",
+        category: "Seminar",
+        date: "December 7, 2025",
+        location: "Java Coffee"
+    },
+    {
+        id:9,
+        image: imag9,
+        name: "Wedding Expo 2025",
+        title: "Discover the latest wedding trends, vendors, and planning services..",
+        category: "Event",
+        date: "August 25, 2025",
+        location: "Armaan Hall"
+    },
+    {
+        id:10,
+        image: imag10,
+        name:"Entrepreneurship Bootcamp",
+        title: "Insentive program for aspring entrepreneurs to launch their startups..",
+        category: "Seminar",
+        date: "March 1, 2025",
+        location: "Innovation Center"
+    },
+    {
+        id:11,
+        image: imag11,
+        name: "Music Festival 2025",
+        title: "Three days of live music featuring local and international artists..",
+        category: "Event",
+        date: "May 04, 2025",
+        location: "Rashka-Prodution"
+    },
+    {
+        id:12,
+        image: imag12,
+        name: "Data Science Workshop",
+        title: "Hands-on training in data analysis, machine learning, and visualization ..",
+        category: "Event",
+        date: "January 25, 2025",
+        location: "University Campus"
+    },
 ]
 
 
