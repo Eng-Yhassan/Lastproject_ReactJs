82c7a5d0d3936d02f1c4d803573c33151c29e534
Hakiimdaud <cabdihakiim395@gmail.com>
2025-08-25 09:27:37 -0700

    add meet team

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import image from "../images/aboutProImg.jpg"
+import img1 from "../images/hakim.jpg"
+import img2 from "../images/yaya.jpg"
+import img3 from "../images/yonka.jpg"
 
 export default function Home() {
   return (
     <div>
       {/* Left Side - Image */}
 
-      <div className='bg-gradient-to-r from-indigo-900 via-purple-800 to-indigo-900 text-white w-screen h-[450px'>
-        <h1 className='text-6xl text-center pt-14 font-bold text-white'>Creating unforgatable <span className='text-orange-400'>Events</span></h1>
-        <p className='w-[60%] text-center ml-[20%] text-white pt-10 text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem delectus aspernatur reiciendis. Autem, blanditiis eos ab praesentium vero obcaecati nam ipsa, cum minima dicta debitis.</p>
+      <div className='bg-gradient-to-r from-indigo-900 via-purple-800 to-indigo-900 text-white w-screen h-[450px]'>
+        <h1 className='sm:text-6xl text-3xl text-center pt-14 font-bold text-white'>Creating unforgatable <span className='text-orange-600'>Events</span></h1>
+        <p className='sm:w-[60%] w-[80%] text-center sm:ml-[20%] ml-[8%] text-white pt-10 sm:text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem delectus aspernatur reiciendis. Autem, blanditiis eos ab praesentium vero obcaecati nam ipsa, cum minima dicta debitis.</p>
         <div className='flex gap-4 mt-10'>
-          <button className='bg-orange-400 font-semibold p-2 py-3 ml-[38%] px-6 rounded-lg'>Explore Events</button>
+          <button className='bg-orange-600 font-semibold p-2 py-3 sm:ml-[38%] ml-[8%] px-6 rounded-lg'>Explore Events</button>
           <button className='text-white font-semibold p-2 py-3 border-2 border-white px-6 rounded-lg'>Get in Touch</button>
         </div>
       </div>
@@ -19,25 +22,24 @@ export default function Home() {
 
 
       <div className='mt-20 mb-20'>
-        <h1 className='text-5xl text-center font-bold'>Welcome To Event Pro</h1>
-        <p className='w-[78%] text-center ml-[14%] pt-7 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed minima est dignissimos. Itaque minima perspiciatis, iusto eligendi ullam inventore in, laboriosam autem sint dolre minima Itaque minima perspiciatis reiciendis nobis necessitatibus nesciunt, eum impedit!</p>
+        <h1 className='sm:text-5xl text-3xl text-center font-bold'>Welcome To Event Pro</h1>
+        <p className='w-[78%] text-center sm:ml-[14%] ml-[10%] pt-7 sm:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed minima est dignissimos. Itaque minima perspiciatis, iusto eligendi ullam inventore in, laboriosam autem sint dolre minima Itaque minima perspiciatis reiciendis nobis necessitatibus nesciunt, eum impedit!</p>
         <div>
-
         </div>
       </div>
-      
 
-      <div className='flex px-10 gap-8'>
-        <div>
-          <h2 className="text-5xl font-bold mb-4">About EventPro</h2>
-          <p className="text-gray-600 mb-6">With over a decade of experience in the events industry, EventPro has established itself asa partner for organizations seeking to create impactful and memorable experiences.</p>
+
+      <div className='sm:flex px-10 gap-8 sm:space-y-0 space-y-10 bg-slate-100 items-center'>
+        <div className='pt-12 w-screen h-[510px]'>
+          <h2 className="sm:text-5xl text-3xl font-bold mb-4 sm:text-start text-center sm:-ml-0 -ml-10">About EventPro</h2>
+          <p className="text-gray-600 sm:mb-6 sm:w-0 w-[90%]">With over a decade of experience in the events industry, EventPro has established itself asa partner for organizations seeking to create impactful and memorable experiences.</p>
 
           <ul className="space-y-4">
             <li className="flex items-start">
               <i className="fa-solid fa-check text-green-500 mt-1 text-lg"></i>
               <div className="ml-3">
                 <h3 className="font-semibold">Comprehensive Services</h3>
-                <p className="text-gray-600">From concept to execution, we handle every aspect of your event.</p>
+                <p className="text-gray-600 sm:w-0 w-[90%]">From concept to execution, we handle every aspect of your event.</p>
               </div>
             </li>
 
@@ -45,7 +47,7 @@ export default function Home() {
               <i className="fa-solid fa-check text-green-500 mt-1 text-lg"></i>
               <div className="ml-3">
                 <h3 className="font-semibold">Innovative Solutions</h3>
-                <p className="text-gray-600">Creative approaches that make your events stand out and engage audiences.</p>
+                <p className="text-gray-600 sm:w-0 w-[90%]">Creative approaches that make your events stand out and engage audiences.</p>
               </div>
             </li>
 
@@ -53,19 +55,69 @@ export default function Home() {
               <i className="fa-solid fa-check text-green-500 mt-1 text-lg"></i>
               <div className="ml-3">
                 <h3 className="font-semibold">Proven Track Record</h3>
-                <p className="text-gray-600">Successfully delivered over 500 events with 98% client satisfaction rate.</p>
+                <p className="text-gray-600 sm:w-0 w-[90%]">Successfully delivered over 500 events with 98% client satisfaction rate.</p>
               </div>
             </li>
           </ul>
+          <button className='bg-indigo-800 text-white font-semibold p-2 py-3 px-6 rounded-lg mt-6'>Learn More About Us</button>
+
         </div>
 
         {/* Right Side - Image */}
         <div>
-          <img
-            src={image}
-            alt="Meeting"
-            className="rounded-2xl shadow-lg w-[1200px] h-[400px]"
+          <img src={image} alt="Meeting" className="rounded-2xl shadow-lg sm:w-[1200px] sm:h-[400px]"
           />
+        </div>
+      </div>
+
+
+      {/* Meet Team  */}
+
+
+      <div className="mt-20 mb-20">
+        {/* Section Title */}
+        <h1 className="sm:text-5xl text-3xl text-center font-bold">Meet Our Team</h1>
+        <p className="w-[78%] text-center sm:ml-[14%] ml-[10%] pt-7 sm:text-xl text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed minima est dignissimos.
+          Itaque minima perspiciatis, iusto eligendi ullam inventis nobis necessitatibus nesciunt, eum impedit!
+        </p>
+
+        {/* Team Grid */}
+        <div className="mt-12 grid sm:grid-cols-3 gap-8 px-6 sm:px-20">
+
+          {/* Team Member 1 Abdihakim*/}
+          <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:shadow-2xl transition">
+            <img src={img1} alt="Abdihakiim" className="rounded-2xl w-full h-64 object-cover" />
+
+            <h2 className="mt-4 text-xl font-semibold">Abdihakim A Daud</h2>
+            <p className="text-indigo-600 font-medium">Director</p>
+            <p className="mt-2 text-gray-600 text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste obcaecati distinctio ex!
+            </p>
+          </div>
+
+          {/* Team Member 2 Yerman */}
+          <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:shadow-2xl transition">
+            <img src={img2} alt="Ayaan" className="rounded-2xl w-full h-64 object-cover" />
+            <h2 className="mt-4 text-xl font-semibold">Yahye Hassan</h2>
+            <p className="text-indigo-600 font-medium">Manager</p>
+            <p className="mt-2 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste obcaecati distinctio ex!</p>
+          </div>
+
+          {/* Team Member 3 Yoonis */}
+          <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:shadow-2xl transition">
+            <img src={img3} alt="Mahad" className="rounded-2xl w-full h-64 object-cover" />
+            <h2 className="mt-4 text-xl font-semibold">Yoonis Gallad</h2>
+            <p className="text-indigo-600 font-medium">Event Planner</p>
+            <p className="mt-2 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste obcaecati distinctio ex!</p>
+          </div>
+
+        </div>
+      </div>
+      <div className='mt-20 mb-20'>
+        <h1 className='sm:text-5xl text-3xl text-center font-bold'>Featured Events</h1>
+        <p className='w-[78%] text-center sm:ml-[14%] ml-[10%] pt-7 sm:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed minima est dignissimos. Itaque minima </p>
+        <div>
         </div>
       </div>
 
