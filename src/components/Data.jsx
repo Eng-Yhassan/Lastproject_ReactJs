92030388efb09de380bc71e334644ebd29615112
Yoonis <yoonkagallad2021@gmail.com>
2025-08-25 06:15:09 +0300

    last-project

---
+++ src/components/Data.jsx
@@ -0,0 +1,56 @@
+import imag1 from "../images/tech.jpg"
+import imag2 from "../images/Leadership.jpg"
+import imag3 from "../images/Charity-Gala.jpg"
+import imag4 from "../images/Digital.webp"
+import imag5 from "../images/Corporate.jpg"
+
+const Data= [
+    {
+        id:1,
+        image: imag1,
+        name: "Tech Innovation Summit 2025",
+        title: "join industry leaders for groundbreaking discussion on emerging technologies and...",
+        category: "Events",
+        date: "March 15, 2025",
+        location: "Wadajir-Hotel-Jaziira"
+    },
+    {
+        id:2,
+        image: imag2,
+        name: "Leadership Excellence Seminar",
+        title: "Develop essential leadership Skills with expert facilitators and interactive workshop.",
+        category: "Seminar",
+        date: "February 15, 2025",
+        location: "Garowe-Putland,"
+    },
+    {
+        id:3,
+        image: imag3,
+        name: "Annual Charity Gala",
+        title: "An elegant evening supporting local communities with entertainment, dining, and....",
+        category: "Events",
+        date: "July 20, 2025",
+        location: "Hodan-Ex-digfeer-hospital"
+    },
+    {
+        id:4,
+        image: imag4,
+        name: "Digital Marketting Workshop",
+        title: "Master the latest digital marketting strategies and tools for business growth,",
+        category: "Seminar",
+        date: "Jun 15, 2025",
+        location: "km4"
+    },
+    {
+        id:5,
+        image: imag5,
+        name: "Corporate Team Building Retreat",
+        title: "Strengthen team bonds with outdoor activities and professional development...",
+        category: "Events",
+        date: "April 23, 2025",
+        location: "Howl-Wadaag"
+    }
+]
+
+
+export default Data
